063dd0c11324dff7d4171f254b8aa2966586d5ec
David Gelman <gelmands@college.omsu.ru>
2026-02-14 15:40:23 +0600

    changed UUIDEnrichFilter to implement Filter interface

--- src/main/java/ru/gelman/user_crud_service/filter/UUIDEnrichFilter.java
+++ src/main/java/ru/gelman/user_crud_service/filter/UUIDEnrichFilter.java
@@ -1,26 +1,25 @@
 package ru.gelman.user_crud_service.filter;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
+import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.UUID;
 
 @Slf4j
 @Component
-public class UUIDEnrichFilter extends OncePerRequestFilter {
+public class UUIDEnrichFilter implements Filter {
     private static final String UUID_HEADER_NAME = "x-uuid";
 
     @Override
-    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         String uuid = UUID.randomUUID().toString();
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
         try {
             request.setAttribute(UUID_HEADER_NAME, uuid);
             response.setHeader(UUID_HEADER_NAME, uuid);
